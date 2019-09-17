e398ede4ac21db4adfee118300c9fcab639e2269
Dalgona <dalgona@hontou.moe>
2019-09-17 22:30:45 +0900

    feat: implement the bitmap-to-outline glyph builder DSL

---
+++ lib/pixel_font/bmp_glyphs.ex
@@ -0,0 +1,52 @@
+defmodule PixelFont.BMPGlyphs do
+  alias PixelFont.RectilinearShape
+  alias PixelFont.RectilinearShape.Path
+
+  defmacro __using__(_opts) do
+    quote do
+      import unquote(__MODULE__)
+
+      @glyphs []
+    end
+  end
+
+  defmacro glyph([{type, id}], do: block) when type in ~w(unicode name)a do
+    attrs =
+      block
+      |> elem(2)
+      |> Enum.filter(&match?({a, _, [_]} when is_atom(a), &1))
+      |> Enum.map(fn {name, _, [expr]} ->
+        {name, expr |> Code.eval_quoted() |> elem(0)}
+      end)
+      |> Map.new()
+
+    contours =
+      attrs.data
+      |> String.split(~r/\r?\n/, trim: true)
+      |> Enum.map(&to_charlist/1)
+      |> RectilinearShape.from_bmp()
+      |> Path.transform({{1, 0}, {0, -1}}, {attrs.xmin, attrs.ymax})
+
+    item =
+      quote do
+        %{
+          type: unquote(type),
+          id: unquote(id),
+          contours: unquote(contours)
+        }
+        |> Map.merge(unquote(Macro.escape(attrs)))
+      end
+
+    quote do
+      @glyphs [unquote(item) | @glyphs]
+    end
+  end
+
+  defmacro export_glyphs do
+    quote do
+      @glyphs Enum.reverse(@glyphs)
+
+      def glyphs, do: @glyphs
+    end
+  end
+end
