6f1c7a01652c8fba3237753bae70ba90695efc47
Dalgona <me@dalgona.dev>
2021-01-11 21:49:31 +0900

    feat: add a DSL for defining GSUB single substitution lookups

---
+++ lib/pixel_font/dsl/otf_layout/lookups/gsub.ex
@@ -0,0 +1,77 @@
+defmodule PixelFont.DSL.OTFLayout.Lookups.GSUB do
+  @moduledoc false
+
+  require PixelFont.Util
+  import PixelFont.DSL.MacroHelper
+  import PixelFont.Util
+  alias PixelFont.Glyph
+  alias PixelFont.TableSource.GSUB
+  alias PixelFont.TableSource.GSUB.Single1
+  alias PixelFont.TableSource.GSUB.Single2
+  alias PixelFont.TableSource.OTFLayout.GlyphCoverage
+  alias PixelFont.TableSource.OTFLayout.Lookup
+
+  @doc false
+  @spec __import_items__() :: [{atom(), arity()}]
+  def __import_items__ do
+    [
+      single_substitution: 2
+    ]
+  end
+
+  defmacro single_substitution(name, do: do_block) do
+    quote do
+      fn ->
+        import unquote(__MODULE__), only: [substitutions: 1]
+
+        %Lookup{
+          owner: GSUB,
+          type: 1,
+          name: unquote(name),
+          subtables:
+            unquote(get_exprs(do_block))
+            |> List.flatten()
+            |> Enum.reject(&is_nil/1)
+            |> Enum.map(& &1.())
+        }
+      end
+    end
+  end
+
+  defmacro substitutions(do: do_block) do
+    quote do
+      fn ->
+        import unquote(__MODULE__), only: [substitute: 2]
+
+        unquote(__MODULE__).__make_single_subtable__(unquote(get_exprs(do_block)))
+      end
+    end
+  end
+
+  @spec substitute(Macro.t(), Macro.t()) :: Macro.t()
+  defmacro substitute(from_id, to_id), do: {from_id, to_id}
+
+  @doc false
+  @spec __make_single_subtable__([{Glyph.id(), Glyph.id()}]) :: Single1.t() | Single2.t()
+  def __make_single_subtable__(substitutions) do
+    subst_gids =
+      substitutions
+      |> List.flatten()
+      |> Enum.reject(&is_nil/1)
+      |> Enum.map(fn {from, to} -> {gid!(from), gid!(to)} end)
+
+    subst_gids
+    |> Enum.map(fn {from_gid, to_gid} -> to_gid - from_gid end)
+    |> Enum.uniq()
+    |> case do
+      [diff] ->
+        %Single1{
+          gids: subst_gids |> Enum.map(&elem(&1, 0)) |> GlyphCoverage.of(),
+          gid_diff: diff
+        }
+
+      [_ | _] ->
+        %Single2{substitutions: subst_gids}
+    end
+  end
+end
---
+++ lib/pixel_font/otf_layout.ex
@@ -0,0 +1,37 @@
+defmodule PixelFont.OTFLayout do
+  @moduledoc false
+
+  import PixelFont.DSL.MacroHelper
+
+  defmacro __using__(_opts) do
+    quote do
+      import unquote(__MODULE__), only: [lookups: 3]
+    end
+  end
+
+  @spec lookups(module(), [for: <<_::32>>], do: Macro.t()) :: Macro.t()
+  defmacro lookups(name, [for: type], do: do_block) when type in ~w(GPOS GSUB) do
+    import_module = Module.concat(PixelFont.DSL.OTFLayout.Lookups, type)
+
+    quote do
+      defmodule unquote(name) do
+        require unquote(import_module)
+        import unquote(__MODULE__), only: []
+        import unquote(import_module), only: unquote(import_module.__import_items__())
+        alias PixelFont.TableSource.OTFLayout.Lookup
+        alias PixelFont.TableSource.OTFLayout.LookupList
+
+        @spec lookups() :: [LookupList.t()]
+        def lookups do
+          %LookupList{
+            lookups:
+              unquote(get_exprs(do_block))
+              |> List.flatten()
+              |> Enum.reject(&is_nil/1)
+              |> Enum.map(& &1.())
+          }
+        end
+      end
+    end
+  end
+end
