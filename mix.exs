d2dbbc69b16f00e1f170766790de34e536d1e200
Dalgona <me@dalgona.dev>
2021-01-16 15:51:05 +0900

    build: add dialyxir to dev dependencies

--- mix.exs
+++ mix.exs
@@ -6,11 +6,16 @@ defmodule PixelFont.Mixfile do
       app: :pixel_font,
       version: "0.1.0",
       elixir: "~> 1.11",
-      deps: deps()
+      deps: deps(),
+      dialyzer: [
+        plt_add_apps: [:mix]
+      ]
     ]
   end
 
   defp deps do
-    []
+    [
+      {:dialyxir, "~> 1.0", only: :dev, runtime: :false}
+    ]
   end
 end
