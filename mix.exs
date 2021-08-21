0b2e2d10b60bfc2b6a049d899d046abf34e5f7cb
Dalgona <me@dalgona.dev>
2021-08-21 14:13:52 +0900

    build: add Mox test dependency

--- mix.exs
+++ mix.exs
@@ -16,14 +16,20 @@ defmodule PixelFont.Mixfile do
         "coveralls.detail": :test,
         "coveralls.post": :test,
         "coveralls.html": :test
-      ]
+      ],
+      elixirc_paths: elixirc_paths(Mix.env())
     ]
   end
+
+  defp elixirc_paths(env)
+  defp elixirc_paths(:test), do: ["test/support", "lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.14", only: :test}
+      {:excoveralls, "~> 0.14", only: :test},
+      {:mox, "~> 1.0", only: :test}
     ]
   end
 end
