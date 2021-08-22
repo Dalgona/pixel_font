7730432f4790ded60948d1989783e3912116a917
Dalgona <me@dalgona.dev>
2021-08-22 09:10:27 +0900

    test: add tests for ChainedSequenceContext3

---
+++ test/pixel_font/table_source/otf_layout/chained_sequence_context_3_test.exs
@@ -0,0 +1,50 @@
+defmodule PixelFont.TableSource.OTFLayout.ChainedSequenceContext3Test do
+  use ExUnit.Case, async: true
+  import Mox
+  alias PixelFont.Glyph
+  alias PixelFont.TableSource.OTFLayout.ChainedSequenceContext3
+  alias PixelFont.TableSource.OTFLayout.GlyphCoverage
+
+  describe "compile/2" do
+    setup [:setup_mock, :verify_on_exit!]
+
+    test "properly compiles chained sequence context subtable format 3" do
+      subtable = %ChainedSequenceContext3{
+        backtrack: [GlyphCoverage.of('abc')],
+        input: [GlyphCoverage.of('def'), GlyphCoverage.of('ghi')],
+        lookahead: [GlyphCoverage.of('jkl')],
+        lookup_records: [{0, "Lookup 1"}, {1, "Lookup 2"}]
+      }
+
+      lookup_indices = %{"Lookup 1" => 10, "Lookup 2" => 20}
+
+      compiled_subtable =
+        ChainedSequenceContext3.compile(subtable, lookup_indices: lookup_indices)
+
+      expected =
+        [
+          [3, 1, 26, 2, 36, 46, 1, 56, 2],
+          # Lookup records
+          [[0, 10], [1, 20]],
+          # Backtrack coverage tables
+          [[2, 1, ?a, ?c, 0]],
+          # Input coverage tables
+          [[2, 1, ?d, ?f, 0], [2, 1, ?g, ?i, 0]],
+          # Lookahead coverage tables
+          [[2, 1, ?j, ?l, 0]]
+        ]
+        |> List.flatten()
+        |> Enum.map(&<<&1::16>>)
+        |> IO.iodata_to_binary()
+
+      assert compiled_subtable === expected
+    end
+  end
+
+  defp setup_mock(_) do
+    PixelFont.GlyphStorage.Mock
+    |> expect(:get, 12, &%Glyph{gid: &1})
+
+    :ok
+  end
+end
