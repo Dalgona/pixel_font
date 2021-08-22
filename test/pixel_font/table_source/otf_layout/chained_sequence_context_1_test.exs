2dd576aea10714938ba01646af1ff190c9e73ce3
Dalgona <me@dalgona.dev>
2021-08-22 09:29:40 +0900

    test: add tests for ChainedSequenceContext1

---
+++ test/pixel_font/table_source/otf_layout/chained_sequence_context_1_test.exs
@@ -0,0 +1,57 @@
+defmodule PixelFont.TableSource.OTFLayout.ChainedSequenceContext1Test do
+  use ExUnit.Case, async: true
+  import Mox
+  alias PixelFont.Glyph
+  alias PixelFont.TableSource.OTFLayout.ChainedSequenceContext1
+
+  describe "compile/2" do
+    setup [:setup_mock, :verify_on_exit!]
+
+    test "properly compiles chained sequence context subtable format 1" do
+      subtable = %ChainedSequenceContext1{
+        rulesets: %{
+          ?a => [
+            %{
+              backtrack: 'zyx',
+              input: 'bc',
+              lookahead: 'def',
+              lookup_records: [{0, "Lookup 1"}, {2, "Lookup 2"}]
+            }
+          ]
+        }
+      }
+
+      lookup_indices = %{"Lookup 1" => 10, "Lookup 2" => 20}
+
+      compiled_subtable =
+        ChainedSequenceContext1.compile(subtable, lookup_indices: lookup_indices)
+
+      expected =
+        [
+          [1, 8, 1, 14],
+          # Coverage table
+          [1, 1, ?a],
+          # Ruleset tables
+          [
+            [
+              [1, 4],
+              # Rule tables
+              [[3, 'zyx', 3, 'bc', 3, 'def', 2, 0, 10, 2, 20]]
+            ]
+          ]
+        ]
+        |> List.flatten()
+        |> Enum.map(&<<&1::16>>)
+        |> IO.iodata_to_binary()
+
+      assert compiled_subtable === expected
+    end
+  end
+
+  defp setup_mock(_) do
+    PixelFont.GlyphStorage.Mock
+    |> expect(:get, 9, &%Glyph{gid: &1})
+
+    :ok
+  end
+end
