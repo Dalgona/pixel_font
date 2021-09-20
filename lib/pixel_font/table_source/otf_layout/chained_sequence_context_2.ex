b5c1a43e064fc91c265cb5bfc507eb46cbca59d9
Dalgona <me@dalgona.dev>
2021-09-20 20:20:41 +0900

    feat: implement ChainedSequenceContext2

---
+++ lib/pixel_font/table_source/otf_layout/chained_sequence_context_2.ex
@@ -0,0 +1,161 @@
+defmodule PixelFont.TableSource.OTFLayout.ChainedSequenceContext2 do
+  alias PixelFont.TableSource.GPOSGSUB
+  alias PixelFont.TableSource.OTFLayout.ClassDefinition
+  alias PixelFont.TableSource.OTFLayout.GlyphCoverage
+  alias PixelFont.TableSource.OTFLayout.Lookup
+  alias PixelFont.Util
+
+  defstruct backtrack_classes: %ClassDefinition{},
+            input_classes: %ClassDefinition{},
+            lookahead_classes: %ClassDefinition{},
+            rulesets: %{}
+
+  @type t :: %__MODULE__{
+          backtrack_classes: ClassDefinition.t(),
+          input_classes: ClassDefinition.t(),
+          lookahead_classes: ClassDefinition.t(),
+          rulesets: rulesets()
+        }
+
+  @type rulesets :: %{optional(integer()) => ruleset()}
+  @type ruleset :: [rule()]
+
+  @type rule :: %{
+          backtrack: [integer()],
+          input: [integer()],
+          lookahead: [integer()],
+          lookup_records: [{integer(), Lookup.id()}]
+        }
+
+  @spec compile(t(), keyword()) :: binary()
+  def compile(subtable, opts) do
+    lookup_indices = opts[:lookup_indices]
+    input_classes = subtable.input_classes
+
+    coverage =
+      input_classes.assignments
+      |> Map.values()
+      |> GlyphCoverage.of()
+      |> GlyphCoverage.compile()
+
+    max_input_class = input_classes.assignments |> Map.keys() |> Enum.max()
+    coverage_offset = 14 + max_input_class * 2
+    class_def_offset_base = coverage_offset + byte_size(coverage)
+
+    {ruleset_offset_base, class_def_offsets, compiled_class_defs} =
+      ~w(backtrack_classes input_classes lookahead_classes)a
+      |> Enum.map(&Map.get(subtable, &1))
+      |> Util.offsetted_binaries(class_def_offset_base, &ClassDefinition.compile/1)
+
+    {ruleset_offsets, compiled_rulesets} =
+      compile_class_rulesets(
+        subtable.rulesets,
+        ruleset_offset_base,
+        max_input_class,
+        lookup_indices
+      )
+
+    IO.iodata_to_binary([
+      # format
+      <<2::16>>,
+      # coverageOffset
+      <<coverage_offset::16>>,
+      # {backtrack,input,lookahead}ClassDefOffset
+      class_def_offsets,
+      # chainedClassSeqRuleSetCount
+      <<max_input_class + 1::16>>,
+      # chainedClassSeqRuleSetOffsets[]
+      ruleset_offsets,
+      # Coverage table
+      coverage,
+      # {Backtrack,Input,Lookahead} class definition tables
+      compiled_class_defs,
+      # Chained class sequence ruleset tables
+      compiled_rulesets
+    ])
+  end
+
+  @spec compile_class_rulesets(
+          rulesets(),
+          non_neg_integer(),
+          non_neg_integer(),
+          GPOSGSUB.lookup_indices()
+        ) :: {iodata(), iodata()}
+  defp compile_class_rulesets(rulesets, offset_base, max_input_class, lookup_indices) do
+    compiled_rulesets =
+      0..max_input_class
+      |> Enum.map(&rulesets[&1])
+      |> Enum.map(&compile_class_ruleset(&1, lookup_indices))
+
+    ruleset_offsets =
+      compiled_rulesets
+      |> Enum.reduce({offset_base, []}, fn
+        "", {pos, offsets} -> {pos, [<<0::16>> | offsets]}
+        ruleset, {pos, offsets} -> {pos + byte_size(ruleset), [<<pos::16>> | offsets]}
+      end)
+      |> elem(1)
+      |> Enum.reverse()
+
+    {ruleset_offsets, compiled_rulesets}
+  end
+
+  @spec compile_class_ruleset(ruleset() | nil, GPOSGSUB.lookup_indices()) :: iodata()
+  defp compile_class_ruleset(rules, lookup_indices)
+  defp compile_class_ruleset(nil, _lookup_indices), do: ""
+
+  defp compile_class_ruleset(rules, lookup_indices) do
+    rule_count = length(rules)
+    rule_offset_base = 2 + rule_count * 2
+
+    {_, offsets, compiled_rules} =
+      Util.offsetted_binaries(rules, rule_offset_base, &compile_class_rule(&1, lookup_indices))
+
+    IO.iodata_to_binary([
+      # chainedClassSeqRuleCount
+      <<rule_count::16>>,
+      # chainedClassSeqRuleOffsets[]
+      offsets,
+      # Chained class sequence rule tables
+      compiled_rules
+    ])
+  end
+
+  @spec compile_class_rule(rule(), GPOSGSUB.lookup_indices()) :: iodata()
+  defp compile_class_rule(rule, lookup_indices) do
+    compiled_lookup_records =
+      Enum.map(rule.lookup_records, fn {glyph_pos, lookup_id} ->
+        <<glyph_pos::16, lookup_indices[lookup_id]::16>>
+      end)
+
+    [
+      # backtrackGlyphCount
+      <<length(rule.backtrack)::16>>,
+      # backtrackSequence[]
+      Enum.map(rule.backtrack, &<<&1::16>>),
+      # inputGlyphCount
+      <<length(rule.input) + 1::16>>,
+      # inputSequence[]
+      Enum.map(rule.input, &<<&1::16>>),
+      # lookaheadGlyphCount
+      <<length(rule.lookahead)::16>>,
+      # lookaheadSequence[]
+      Enum.map(rule.lookahead, &<<&1::16>>),
+      # seqLookupCount
+      <<length(compiled_lookup_records)::16>>,
+      # seqLookupRecords[]
+      compiled_lookup_records
+    ]
+  end
+
+  defimpl PixelFont.TableSource.GPOS.Subtable do
+    alias PixelFont.TableSource.OTFLayout.ChainedSequenceContext2
+
+    defdelegate compile(subtable, opts), to: ChainedSequenceContext2
+  end
+
+  defimpl PixelFont.TableSource.GSUB.Subtable do
+    alias PixelFont.TableSource.OTFLayout.ChainedSequenceContext2
+
+    defdelegate compile(subtable, opts), to: ChainedSequenceContext2
+  end
+end
---
+++ test/pixel_font/table_source/otf_layout/chained_sequence_context_2_test.exs
@@ -0,0 +1,67 @@
+defmodule PixelFont.TableSource.OTFLayout.ChainedSequenceContext2Test do
+  use PixelFont.Case, async: true
+  alias PixelFont.Glyph
+  alias PixelFont.TableSource.OTFLayout.ClassDefinition
+  alias PixelFont.TableSource.OTFLayout.ChainedSequenceContext2
+
+  describe "compile/2" do
+    setup [:setup_mock, :verify_on_exit!]
+
+    test "properly compiles chained sequence context subtable format 2" do
+      subtable = %ChainedSequenceContext2{
+        backtrack_classes: %ClassDefinition{assignments: %{1 => 'abc'}},
+        input_classes: %ClassDefinition{assignments: %{1 => 'def'}},
+        lookahead_classes: %ClassDefinition{assignments: %{1 => 'ghi'}},
+        rulesets: %{
+          1 => [
+            %{
+              backtrack: [1],
+              input: [1],
+              lookahead: [1],
+              lookup_records: [{0, "Lookup 1"}, {1, "Lookup 2"}]
+            }
+          ]
+        }
+      }
+
+      lookup_indices = %{"Lookup 1" => 10, "Lookup 2" => 20}
+
+      compiled_subtable =
+        ChainedSequenceContext2.compile(subtable, lookup_indices: lookup_indices)
+
+      expected =
+        to_wordstring([
+          [2, 16, 26, 36, 46, 2, [0, 56]],
+          # Coverage table
+          [2, 1, [?d, ?f, 0]],
+          # Class definitions
+          [
+            [2, 1, [?a, ?c, 1]],
+            [2, 1, [?d, ?f, 1]],
+            [2, 1, [?g, ?i, 1]]
+          ],
+          # Ruleset tables
+          [
+            # Ruleset for input class 0 (empty)
+            [],
+            # Ruleset for input class 1
+            [
+              [1, [4]],
+              [
+                [1, [1], 2, [1], 1, [1], 2, [[0, 10], [1, 20]]]
+              ]
+            ]
+          ]
+        ])
+
+      assert compiled_subtable === expected
+    end
+  end
+
+  defp setup_mock(_) do
+    PixelFont.GlyphStorage.Mock
+    |> expect(:get, 12, &%Glyph{gid: &1})
+
+    :ok
+  end
+end
