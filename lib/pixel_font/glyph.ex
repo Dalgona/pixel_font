91ee403dd7b8c6f7181d75190a6178c800b2b5a4
Dalgona <me@dalgona.dev>
2021-12-14 22:37:14 +0900

    feat: add :variations field to Glyph struct

--- lib/pixel_font/glyph.ex
+++ lib/pixel_font/glyph.ex
@@ -1,10 +1,17 @@
 defmodule PixelFont.Glyph do
   alias PixelFont.Glyph.BitmapData
   alias PixelFont.Glyph.CompositeData
+  alias PixelFont.Glyph.VariationSequence
 
-  defstruct id: 0, gid: 0, data: %BitmapData{}
+  defstruct id: 0, gid: 0, data: %BitmapData{}, variations: false
 
-  @type t :: %__MODULE__{id: id(), gid: non_neg_integer(), data: data()}
+  @type t :: %__MODULE__{
+          id: id(),
+          gid: non_neg_integer(),
+          data: data(),
+          variations: VariationSequence.t() | false
+        }
+
   @type id :: non_neg_integer() | binary()
   @type data :: BitmapData.t() | CompositeData.t()
 end
