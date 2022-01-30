f7af3544a34109a28e7a88dd70f2991ceb27ad4a
Dalgona <me@dalgona.dev>
2022-01-30 12:24:51 +0900

    feat: add "pixel_font.build" Mix task

---
+++ lib/mix/tasks/pixel_font/build.ex
@@ -0,0 +1,30 @@
+defmodule Mix.Tasks.PixelFont.Build do
+  use Mix.Task
+  alias PixelFont.Builder
+  alias PixelFont.Font
+  alias PixelFont.TableSource.Name.Definitions, as: Defs
+
+  @impl true
+  def run(args) do
+    mix_project = Mix.Project.get!().project()
+    pixel_font_opts = Keyword.fetch!(mix_project, :pixel_font)
+    font_module = Keyword.fetch!(pixel_font_opts, :font_module)
+    %Font{} = font = font_module.font(args)
+    ttf = Builder.build_ttf(font)
+
+    "#{output_filename(font)}.ttf"
+    |> Path.expand(File.cwd!())
+    |> File.open([:write, :binary], &IO.binwrite(&1, ttf))
+  end
+
+  defp output_filename(%Font{} = font) do
+    font.name_table
+    |> hd()
+    |> Map.get(:records)
+    |> Enum.find(&(elem(&1, 0) === Defs.name_id(:postscript_name)))
+    |> case do
+      nil -> "font"
+      font_name when is_binary(font_name) -> font_name
+    end
+  end
+end
